7783d184f082582334db33145b073775ab71baec
Cabrera Correa Adan <alu2578@cc199.(none)>
2014-10-17 16:00:35 +0100

    Se añade suma, resta, multiplicacion y division

--- lib/complejos.rb
+++ lib/complejos.rb
@@ -8,9 +8,21 @@ class Complejos
 	end
 
 	def to_s
-		"#{@real}+#{@imaginaria}"
+		"#{@real}+#{@imaginaria}i"
 	end
 	def +(other)
-		Complejos.new(@real+other.real,@imaginaria+other.imaginaria)
+		Complejos.new(@real + other.real, @imaginaria + other.imaginaria)
+	end
+	def -(other)
+		Complejos.new(@real - other.real, @imaginaria - other.imaginaria) 
+	end
+	def *(other)
+		Complejos.new(@real * other.real - @imaginaria * other.imaginaria, 
+			@real * other.imaginaria + @imaginaria * other.real)
+	end
+	def /(other)
+		denominador = (other.real * other.real - other.imaginaria * other.imaginaria).to_f
+		Complejos.new((@real * other.real - @imaginaria * other.imaginaria)) / denominador, 
+			(@imaginaria * other.real . @real * other.imaginaria) / denominador)
 	end
 end
