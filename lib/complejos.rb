8e5118eff9ba399a0a11e3eef071d046de03faff
Adan Cabrera <alu0100003956@ull.edu.es>
2014-10-17 16:15:05 +0100

    Se agrega el producto escalar

--- lib/complejos.rb
+++ lib/complejos.rb
@@ -11,18 +11,28 @@ class Complejos
 		"#{@real}+#{@imaginaria}i"
 	end
 	def +(other)
-		Complejos.new(@real + other.real, @imaginaria + other.imaginaria)
+		if (other.is_a? Complejos)
+			Complejos.new(@real + other.real, @imaginaria + other.imaginaria)
+		end
 	end
 	def -(other)
-		Complejos.new(@real - other.real, @imaginaria - other.imaginaria) 
+		if (other.is_a? Complejos)
+			Complejos.new(@real - other.real, @imaginaria - other.imaginaria) 
+		end
 	end
 	def *(other)
-		Complejos.new(@real * other.real - @imaginaria * other.imaginaria, 
-			@real * other.imaginaria + @imaginaria * other.real)
+		if (other.is_a? Complejos)
+			Complejos.new(@real * other.real - @imaginaria * other.imaginaria, 
+				@real * other.imaginaria + @imaginaria * other.real)
+		elsif (other.is_a? Numeric)
+			Complejos.new( other * @real, other * @imaginaria)		
+		end
 	end
 	def /(other)
-		denominador = (other.real * other.real - other.imaginaria * other.imaginaria).to_f
-		Complejos.new((@real * other.real - @imaginaria * other.imaginaria)) / denominador, 
-			(@imaginaria * other.real . @real * other.imaginaria) / denominador)
+		if (other.is_a? Complejos)
+			denominador = (other.real * other.real - other.imaginaria * other.imaginaria).to_f
+			Complejos.new((@real * other.real - @imaginaria * other.imaginaria)) / denominador, 
+				(@imaginaria * other.real . @real * other.imaginaria) / denominador)
+		end
 	end
 end
